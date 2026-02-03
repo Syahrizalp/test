62e27785e9413599eed94c1cf512d498fbf8f4d2
Syahrizalp <syahrizalphas@gmail.com>
2026-02-03 13:45:10 +0700

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,12 +39,12 @@
             <div class="container px-4 px-lg-5 h-100">
                 <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
+                        <h1 class="text-white font-weight-bold">KELOMPOK 3</h1>
                         <hr class="divider" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                        <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                        <p class="text-white-75 mb-5">Belajar Framework Laravel dan Bootstrap</p>
+                        <a class="btn btn-primary btn-xl" href="#about">Lihat</a>
                     </div>
                 </div>
             </div>
@@ -54,9 +54,9 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">We've got what you need!</h2>
+                        <h2 class="text-white mt-0">Laravel</h2>
                         <hr class="divider divider-light" />
-                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                        <p class="text-white-75 mb-4">Laravel adalah framework PHP yang dirancang untuk membantu pengembang membangun aplikasi web yang aman, terstruktur, dan mudah dikembangkan dengan menyediakan fitur lengkap seperti routing, database ORM, sistem template, dan keamanan bawaan.</p>
                         <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
                     </div>
                 </div>
